0412c8874860498e65c6d5af7f2a5dc1c001db0c
Zhu-Rong Zheng <jenifers001d@gmail.com>
2019-09-17 18:06:08 +1000

    Updates

---
+++ precache-manifest.b81c53aaf5decc6142e166fa2be73885.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a35ba90f3247c04324481cdbb83929d2",
+    "url": "/MyCalendar-React/index.html"
+  },
+  {
+    "revision": "539662b87722e66704fe",
+    "url": "/MyCalendar-React/static/css/main.60ee674b.chunk.css"
+  },
+  {
+    "revision": "e1a11e448242abcd747a",
+    "url": "/MyCalendar-React/static/js/2.3fe4428d.chunk.js"
+  },
+  {
+    "revision": "539662b87722e66704fe",
+    "url": "/MyCalendar-React/static/js/main.744d1356.chunk.js"
+  },
+  {
+    "revision": "73b49ec0d0fbe18e4f90",
+    "url": "/MyCalendar-React/static/js/runtime~main.527db7ca.js"
+  }
+]);
